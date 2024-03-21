21568270fd8e78329038ebe148bc8d1abd1e31aa
Dónal Murray <donal.murray@parity.io>
2024-03-21 18:57:36 +0700

    Add test (WIP)

--- system-parachains/coretime/coretime-kusama/src/tests.rs
+++ system-parachains/coretime/coretime-kusama/src/tests.rs
@@ -14,25 +14,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use core::time;
-
-use crate::coretime::{BrokerPalletId, CoretimeBurnPotId, PriceAdapter};
-use emulated_integration_tests_common::accounts::ALICE;
+use crate::{
+	coretime::{BrokerPalletId, CoretimeBurnAccount, PriceAdapter},
+	*,
+};
 use frame_support::{
 	assert_ok,
 	traits::{
 		fungible::{Inspect, Mutate},
-		OnInitialize,
+		Get, OnInitialize,
 	},
-	StorageValue,
 };
-use pallet_broker::{AdaptPrice, ConfigRecordOf};
+use pallet_broker::{AdaptPrice, ConfigRecordOf, SaleInfo};
 use parachains_runtimes_test_utils::ExtBuilder;
-use sp_runtime::{traits::One, FixedU64};
-
-use crate::{
-	xcm_config::{BrokerPalletLocation, RelayTreasuryPalletAccount, TreasuryAccount},
-	*,
+use sp_runtime::{
+	traits::{AccountIdConversion, One},
+	FixedU64,
 };
 
 #[test]
@@ -94,17 +91,18 @@ fn advance_to(b: BlockNumber) {
 
 #[test]
 fn bulk_revenue_is_burnt() {
-	const ALICE: &str = "Alice";
+	const ALICE: [u8; 32] = [1u8; 32];
+
 	ExtBuilder::<Runtime>::default()
 		.with_collators(vec![AccountId::from(ALICE)])
 		.with_session_keys(vec![(
 			AccountId::from(ALICE),
 			AccountId::from(ALICE),
-			SessionKeys { aura: AuraId::from(sp_core::ed25519::Public::from_raw(ALICE)) },
+			SessionKeys { aura: AuraId::from(sp_core::sr25519::Public::from_raw(ALICE)) },
 		)])
 		.build()
 		.execute_with(|| {
-			let timeslice_period = <Runtime as pallet_broker::Config>::TimeslicePeriod::get();
+			let timeslice_period: u32 = <Runtime as pallet_broker::Config>::TimeslicePeriod::get();
 			// Configure broker and start sales
 			let config = ConfigRecordOf::<Runtime> {
 				advance_notice: 1,
@@ -116,32 +114,37 @@ fn bulk_revenue_is_burnt() {
 				renewal_bump: Perbill::from_percent(3),
 				contribution_timeout: 1,
 			};
-			assert_ok!(Broker::configure(coretime_root_origin.clone(), config.clone()));
-			assert_ok!(Broker::start_sales(coretime_root_origin.clone(), 100, 1));
+			assert_ok!(Broker::configure(RuntimeOrigin::root(), config.clone()));
+			assert_ok!(Broker::start_sales(RuntimeOrigin::root(), 100, 1));
 
 			let sale_start = SaleInfo::<Runtime>::get().unwrap().sale_start;
 			advance_to(sale_start + timeslice_period * config.interlude_length);
 
 			// Check and set initial balances.
-			let broker_account = Accountd::from(BrokerPalletId);
-			let coretime_burn_pot = Accountd::from(CoretimeBurnPotId);
-			let treasury_account = RelayTreasuryPalletAccount::get();
-			assert_ok!(Balances::mint_into(&AccountId::from(ALICE), 1000));
-			assert_eq!(treasury_account.balance(), 0);
-			assert_eq!(broker_account.balance(), 0);
+			let broker_account = BrokerPalletId::get().into_account_truncating();
+			let coretime_burn_account = CoretimeBurnAccount::get();
+			let treasury_account = xcm_config::RelayTreasuryPalletAccount::get();
+			assert_ok!(Balances::mint_into(
+				&AccountId::from(ALICE),
+				1000 * ExistentialDeposit::get()
+			));
+			let alice_balance_before = Balances::balance(&AccountId::from(ALICE));
+			let treasury_balance_before = Balances::balance(&treasury_account);
+			let broker_balance_before = Balances::balance(&broker_account);
+			let burn_balance_before = Balances::balance(&coretime_burn_account);
 
-			assert_ok!(Broker::purchase(&AccountId::from(ALICE), 500));
+			assert_ok!(Broker::purchase(RuntimeOrigin::signed(AccountId::from(ALICE)), 500));
 			// Alice decreases
-			assert_eq!(Balances::balance(ALICE), 500);
+			assert!(Balances::balance(&AccountId::from(ALICE)) < alice_balance_before);
 			// Treasury balance does not increase
-			assert_eq!(Balances::balance(treasury_account), 0);
+			assert_eq!(Balances::balance(&treasury_account), treasury_balance_before);
 			// Broker pallet account does not increase
-			assert_eq!(Balances::balance(broker_account), 0);
+			assert_eq!(Balances::balance(&broker_account), broker_balance_before);
 			// Coretime burn pot gets the funds
-			assert_eq!(Balances::balance(coretime_burn_pot), 500);
+			assert!(Balances::balance(&coretime_burn_account) > burn_balance_before);
 
-			// They're burnt at the end of the sale.
-			advance_to(sale_start + timeslice_period * config.region_length + 1);
-			assert_eq!(Balances::balance(coretime_burn_pot), 0);
+			// They're burnt at the end of the sale. TODO
+			// advance_to(sale_start + timeslice_period * config.region_length + 1);
+			// assert_eq!(Balances::balance(coretime_burn_account), 0);
 		});
 }
